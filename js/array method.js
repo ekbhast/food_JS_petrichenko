d039be7f2fb6f7a80b264e6cbad52fb2ae901d39
ekbhast <ekbhast@gmail.com>
2024-09-27 16:43:01 +0300

    first commit

---
+++ js/array method.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//filter
+
+// const names = ['Ivsn', 'Ann', 'Ksenia', 'Voldemart'];
+
+// const shortNames = names.filter(function (name){
+//     return name.length < 5; 
+// });
+
+// console.log(shortNames);
